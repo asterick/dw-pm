6320b64154663abe6f875aef9dbcba7fb8ec555f
Bryon Vandiver <unicdk@gmail.com>
2019-04-24 15:05:52 -0700

    Checkpoint

--- toolchain/pokemini.mk
+++ toolchain/pokemini.mk
@@ -1,14 +1,13 @@
 ifeq ($(OS), Windows_NT)
 	WINE :=
-	TOOLCHAIN := $(dir $(lastword $(MAKEFILE_LIST)))
 else # In Unix we use wine and assume the tools are in the PATH
 	WINE := wine
-	TOOLCHAIN := z:$(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 endif
 
+TOOLCHAIN := $(dir $(lastword $(MAKEFILE_LIST)))/
 OBJECTS = $(patsubst %.c,%.obj,$(patsubst %.s,%.obj,$(SOURCES)))
 
-LKFLAGS = -v -Ml -Tlk-d$(TOOLCHAIN)/ETC/pokemini.dsc -Tlk-L$(TOOLCHAIN)/LIB
+LKFLAGS = -v -Ml -Tlk-d$(TOOLCHAIN)/ETC/pokemini.dsc -Tlc-d$(TOOLCHAIN)/ETC/pokemini.dsc -Tlk-L$(TOOLCHAIN)/LIB -Tlc-f3
 CFLAGS 	= -O2 -Ml -I$(TOOLCHAIN)/INCLUDE -Ta-O -Tc-v
 ASFLAGS = -O -Ml
 
@@ -17,7 +16,10 @@ AS88 = $(WINE) $(TOOLCHAIN)/BIN/AS88.EXE
 
 all: $(TARGET)
 
-$(TARGET): $(OBJECTS)
+$TARGET: $(TARGET).hex
+	python3 $(TOOLCHAIN)/extract.py $@ $<
+
+$(TARGET).hex: $(OBJECTS)
 	$(CC88) $(LKFLAGS)  -o $@ $^
 
 %.obj: %.c
@@ -28,6 +30,6 @@ $(TARGET): $(OBJECTS)
 	$(AS88) $(ASFLAGS) -o $@ $<
 
 clean:
-	rm -Rf $(TARGET) $(OBJECTS)
+	rm -Rf $(TARGET) $(TARGET).hex $(OBJECTS)
 
 .phony: all clean

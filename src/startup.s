76db79899baa396cbc4d4ad5e7ff0afcf25ffb65
Bryon Vandiver <unicdk@gmail.com>
2019-08-06 12:10:30 -0700

    Checkpoint

--- src/startup.s
+++ src/startup.s
@@ -30,20 +30,25 @@ __START:
         ;==========================================================================
         ;===================  system initialization  ==============================
         ;==========================================================================
-        LD      SP,#02000h                      ; stack pointer initialize
-        LD      BR,#020h                        ; BR register initialize to I/O area
-        LD      [BR:21h],#0Ch
-        LD      [BR:25h],#080h
-        LD      [BR:80h],#08h
-        LD      [BR:81h],#08h
-        ;LD     [BR:81h],#09h
+        LD      SP, #02000h                      ; stack pointer initialize
+        LD      BR, #020h                        ; BR register initialize to I/O area
+        LD      [BR:21h], #0Ch
+        LD      [BR:25h], #080h
+        LD      [BR:80h], #08h
+        LD      [BR:81h], #08h
+        ;LD     [BR:81h], #09h
 
-        LD      SC, #0h
+        LD      SC, #030h
 
-        LD      [BR:27h],#0FFh
-        LD      [BR:28h],#0FFh
-        LD      [BR:29h],#0FFh
-        LD      [BR:2Ah],#0FFh
+        LD      [BR:27h], #0FFh
+        LD      [BR:28h], #0FFh
+        LD      [BR:29h], #0FFh
+        LD      [BR:2Ah], #0FFh
+
+_loop:  LD      A, [BR:08Ah]
+        LD      [BR:0FEh], #081h
+        LD      [BR:0FFh], A
+        JRS     _loop
 	CARL	__copytable
 	CARL	_main
 	CARL	__exit
